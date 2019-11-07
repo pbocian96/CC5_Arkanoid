d93a2258721bbe4f137235824cd1559073314945
Sabina Topór <sabina.topor20@gmail.com>
2019-11-08 00:30:22 +0100

    brick

--- src/brick.js
+++ src/brick.js
@@ -1,36 +1,31 @@
 import { ctx, cw, ch } from './main';
-class brick{
-    constructor(width,posx,posy) {
-        this.width=width;
-        this.height=width/3;
-        this.posx=posx;
-        this.posy=posy;
-
+class Brick{
+    constructor(rows,columns) {
+        this.rows=rows;
+        this.columns=columns;
+        this.width=100;
+        this.height=35;
     }
-    draw() {
-        ctx.fillStyle = 'black';
-        ctx.fillRect(posx, posy, this.width, this.height);
+    drawOne(x,y) {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(x, y, this.width, this.height);
         }
-    
-}
-const allBricks=[];
-const bricksInRow=10;
-const bricksInColumn=4;
-const space=10;
-const bw=cw/bricksInRow-space;
-let a=10;
-let b=30+bw/3
 
-for (let i=1; i<=bricksInRow; i++) {
-    let brick = new brick(bw, a, 20);
-    brick.draw();
-    allBricks.push(brick);
+    draw(){
+        const space=10;
+        let a=10;
 
-    for(let j=2; j<=bricksInColumn; j++) {
-        let brick = new brick(bw, a, b);
-        brick.draw();
-        allBricks.push(brick);
-        b += b+bw/3+space;
+        for (let i=1; i<=this.columns; i++) {
+            draw.one(a, 20);
+            let b=30+this.height;
+
+            for (let j=2; j<=this.rows; j++) {
+                draw.one(a,b);
+                b += this.height+space;
+            }
+            a +=this.width+space;
+        }
     }
-    a += bw+space;
 }
+
+
