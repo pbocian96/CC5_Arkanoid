86e6810e1306524144789ac00c8e693b344def8d
MarcinD <m4rcin.duda@gmail.com>
2019-11-10 01:01:03 +0100

    Paddle speed

--- src/paddle.js
+++ src/paddle.js
@@ -7,6 +7,10 @@ class Paddle {
     this.height = 30;
     this.spaceFromBorder = 10;
     this.xSpeed = 20;
+    this.friction = 0.9;
+    this.acceleration = 2;
+    this.velocity = 0;
+
     this.image = new Image(); 
     this.image.src = "src/img/paddle_ani_04.png";
     this.cycleLoop = [0, 0, 30, 30, 60, 60, 90, 90, 120, 120, 150, 150, 180, 180, 150, 150, 120, 90, 90, 60, 60, 30, 30];
@@ -17,51 +21,48 @@ class Paddle {
 
     window.addEventListener("keydown", (e) => {
       if(e.keyCode === 37 || e.keyCode === 65) {
-        this.rightPressed = true;
+        this.leftPressed = true;
       }
       if(e.keyCode === 39 || e.keyCode === 68) {
-        this.leftPressed = true;
+        this.rightPressed = true;
       }
     });
 
     window.addEventListener("keyup", (e) => {
       if(e.keyCode === 37 || e.keyCode === 65) {
-        this.rightPressed = false;
+        this.leftPressed = false;
       }
       if(e.keyCode === 39 || e.keyCode === 68) {
-        this.leftPressed = false;
+        this.rightPressed = false;
       }
      });
   }
 
   draw() {
-    // Paddle style
-    /* ctx.fillStyle = 'white';
-    ctx.fillRect(this.x, ch - this.height, this.length, this.height - this.spaceFromBorder);
-    
-    for (let i = 1; i <= 10; i++)
-    {
-      ctx.fillStyle = `rgb(${i*5},${i*15},${i*25})`;
-      ctx.fillRect(this.x + i, ch - this.height + i, this.length - i*2, this.height - this.spaceFromBorder - i*2);
-    } */
-
-    //SPRITE TEST
-    //ctx.drawImage(this.image, this.x,ch - this.height); 
     this.step();
-    
 
     // Paddle movement
-    if(this.rightPressed && this.x > 0) {
-      this.x -= this.xSpeed;
+    if(this.rightPressed && this.velocity < this.xSpeed) {
+        this.velocity+= this.acceleration;
     }
 
-    if(this.leftPressed && this.x < cw - this.length) {
-      this.x += this.xSpeed;
+    if(this.leftPressed && this.velocity > -this.xSpeed) {
+        this.velocity-= this.acceleration;
+    }
+
+    // Paddle velocity
+    this.velocity *= this.friction;
+    this.x += this.velocity;
+    
+    // Border check
+    if (this.x >= cw - this.length){
+      this.x = cw - this.length;
+    } else if (this.x <= 0) {
+      this.x = 0;
     }
   }
 
   step() {
-    
     ctx.drawImage(
       this.image,
       0, 
@@ -76,15 +77,8 @@ class Paddle {
     this.loopIndex++;
     if (this.loopIndex >= this.cycleLoop.length){
       this.loopIndex = 0;
-    }
-    console.log(this.loopIndex);
-
-    
+    } 
   }
-
 }
-
-
-
 
 export default Paddle;
